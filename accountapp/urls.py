50d824974910d56127428a1680e51a300f7227d7
codemkim <inkin61@naver.com>
2021-07-13 15:55:04 +0900

    Qddddd 7/13

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,16 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'), # 주소 이름 / urls 함수 이름 불러오고 / 이림은 새로 정한다
+
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
